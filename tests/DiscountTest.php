d2fdf95121715ef0c925ad05aa26dc754ed0b502
Mohammed Manssour <manssour.mohammed@gmail.com>
2024-12-20 02:11:48 +0400

    adds missing test

---
+++ tests/DiscountTest.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace MissionX\DiscountsEngine\Tests;
+
+use PHPUnit\Framework\Attributes\Test;
+use PHPUnit\Framework\TestCase;
+
+class DiscountTest extends TestCase
+{
+    #[Test]
+    public function it_works() {}
+}
